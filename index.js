84f071b7e5945f37606e110a9074bdbe20463686
salonidhatrak <salonidhatrak123@gmail.com>
2023-01-05 10:39:06 +0530

    jsonr

--- index.js
+++ index.js
@@ -1,63 +1,75 @@
-function getData(){
-    return fetch(`http://localhost:3000/posts`)
-    .then((res)=>{
-        return res.json()
-    })
-}
+// function getData(){
+//     return fetch(`http://localhost:3000/posts`)
+//     .then((res)=>{
+//         return res.json()
+//     })
+// }
 
-function handleData(){
-    getData().then((res)=>{
-        console.log(res)
-        append(res)
-     })
-}
+// function handleData(){
+//     getData().then((res)=>{
+//         console.log(res)
+//         append(res)
+//      })
+// }
 
-handleData();
+// handleData();
 
-function deletefun(id){
-    return fetch (`http://localhost:3000/posts${id}`,{
-        method:"DELETE",
-        headers:{
-            'Content-Type':'application/json'
-        }
+// function deletefun(id){
+//     return fetch (`http://localhost:3000/posts${id}`,{
+//         method:"DELETE",
+//         headers:{
+//             'Content-Type':'application/json'
+//         }
 
-    })
-}
+//     })
+// }
 
-function handledelete(id){
-    deletefun(id).then((res)=>handleData())
-}
+// function handledelete(id){
+//     deletefun(id).then((res)=>handleData())
+// }
 
-function postfunction (data){
-    return fetch(`http://localhost:3000/posts`,{
-        method:"POST",
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(data)
-    })
-}
+// function postfunction (data){
+//     return fetch(`http://localhost:3000/posts`,{
+//         method:"POST",
+//         headers:{
+//             'Content-Type':'application/json'
+//         },
+//         body:JSON.stringify(data)
+//     })
+// }
 
-function handlePost(data){
-    postfunction(data).then((res)=>handleData())
-}
+// function handlePost(data){
+//     postfunction(data).then((res)=>handleData())
+// }
 
-function append(data){
-    document.getElementById('container').innerHTML=null
-    data.forEach((el)=>{
+// function append(data){
+//     document.getElementById('container').innerHTML=null
+//     data.forEach((el)=>{
         
-        let div=document.createElement('div')
+//         let div=document.createElement('div')
 
-        let p = document.createElement('p');
-        p.textContent=el.title;
+//         let p = document.createElement('p');
+//         p.textContent=el.title;
 
-        btn=document.createElement('button')
-        btn.textContent="Delete";
-        btn.addEventListener('click',function(){
-            handledelete(el.id)
-        })
+//         btn=document.createElement('button')
+//         btn.textContent="Delete";
+//         btn.addEventListener('click',function(){
+//             handledelete(el.id)
+//         })
 
-        div.append(p,btn)
-        document.getElementById('container').append(div)
-    })
-}
+//         div.append(p,btn)
+//         document.getElementById('container').append(div)
+//     })
+// }
+
+
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
